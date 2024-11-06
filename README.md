11b273d90ff12071614eff602b52709315d36863
BaghettiGH <trollolol846@gmail.com>
2024-11-06 16:22:38 +0800

    data page content

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
-# Sreamlit Dashboard Template
+# F1 Podium Prediction
+A Streamlit web application that predicts if a driver will finish on the podium (1st,2nd, or 3rd Place) in a Formula 1 Grand Prix Race using their qualifying times and initial position. This application performs **EDA**, **Data Preprocessing**, and **Supervised Machine Learning** to predict instances using **Logistic Regression**.
+
 
 ![Main Page Screenshot](screenshots/main_page_screenshot.png)
 
+
+
+### 🔗 Links:  
+-  [Streamlit Link]()
+-  [Google Colab Notebook](https://colab.research.google.com/drive/1AxRBCJX24u00DtShTovHU3tbSkpzfIt7?usp=sharing)
+
+
+### 📊 Dataset:
+- [Formula 1 World Championship (1950 - 2024)(Kaggle)](https://www.kaggle.com/datasets/rohanrao/formula-1-world-championship-1950-2020)
 This repository serves as a project guide template for my students in **Introduction to Data Science** course for their final project. It contains a Python file `dashboard_template.py` which contains a boilerplate for a Streamlit dashboard.
+### 📖 Pages:
+
+1. `Dataset` - Brief description of the Formula 1 Dataset used in this dashboard.
+2. `EDA` - Exploratory Data Analysis of the F1 Dataset. Highlights relationship of initial positionl, qualifying times and finishing on the podium. Includes bar graphs,histogram, scatter graph, etc.
+3. `Data Cleaning / Pre-processing` - Data cleaning and pre-processing steps such as encoding the species column and splitting the dataset into training and testing sets.
+4. `Machine Learning` - Training Logistic Regression model. This page also includes the model evaluation, feature importance and classification report.
+5. `Prediction` - Prediction page where users can input values to predict if the driver will finish on the podium.
+6. `Conclusion` - Summary of the insights and observations from the EDA and model training.
+
+### 💡 Findings / Insights
+
+Through exploratory data analysis and training of two classification models (`Decision Tree Classifier` and `Random Forest Regressor`) on the **Iris Flower dataset**, the key insights and observations are:
+
+#### 1. 📊 **Dataset Characteristics**:
+
+- 
+- 
+
+#### 2. 📝 **Feature Distributions and Separability**:
+
+- 
+- 
+
+#### 3. 📈 **Model Performance (Decision Tree Classifier)**:
+
+- 
+- 
+
+#### 4. 📈 **Model Performance (Random Forest Regressor)**:
+
+- 
+- 
+
+##### **Conclusion:**
+
+(Text here)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
+import io
 
 import numpy as np
 import pandas as pd
@@ -46,11 +47,14 @@ with st.sidebar:
     if st.button("About", use_container_width=True, on_click=set_page_selection, args=('about',)):
         st.session_state.page_selection = 'about'
     
-    if st.button("Dataset/Data Cleaning", use_container_width=True, on_click=set_page_selection, args=('dataset',)):
+    if st.button("Dataset", use_container_width=True, on_click=set_page_selection, args=('dataset',)):
         st.session_state.page_selection = 'dataset'
 
     if st.button("EDA", use_container_width=True, on_click=set_page_selection, args=('eda',)):
         st.session_state.page_selection = "eda"
+
+    if st.button("Data Cleaning / Pre-processing", use_container_width=True, on_click=set_page_selection, args=('data_cleaning',)):
+        st.session_state.page_selection = "data_cleaning"
 
     if st.button("Machine Learning", use_container_width=True, on_click=set_page_selection, args=('machine_learning',)): 
         st.session_state.page_selection = "machine_learning"
@@ -73,7 +77,35 @@ df_drivers = pd.read_csv('data/drivers.csv')
 df_qualifying = pd.read_csv('data/qualifying.csv', na_values=['\\N',''])
 df_races = pd.read_csv('data/races.csv')
 df_results = pd.read_csv('data/results.csv')
-df_constructors = pd.read_csv('data/constructors.csv')
+
+df_driver_results = pd.merge(df_results,df_drivers[['driverId','driverRef']], on='driverId',how='inner') #merging driver.csv & results.csv
+df_driver_race_results= pd.merge(df_driver_results,df_races[['raceId','name','year']], on='raceId', how='inner') #merging df_driver_results & races.csv
+df_drivers_quali_race_results = pd.merge(df_driver_race_results,df_qualifying[['raceId','driverId','q1','q2','q3']], on=['raceId','driverId'], how='inner') # merging df_driver_race_results & qualifying.csv
+main_column = ['driverRef','name','year','q1','q2','q3','grid','positionOrder'] #selecting columns
+df_main = df_drivers_quali_race_results[main_column]
+df_main.rename(columns={'driverRef':'driver_name',
+                        'name':'race_name',
+                        'year':'race_year',
+                        'grid':'starting_grid_position',
+                        'positionOrder':'finishing_position'},inplace=True) #renaming columsn
+df_main= df_main.sort_values(by='race_year',ascending=False) #sorting by race_year
+df_main.dropna(subset=['q1'], inplace=True)
+df_main = df_main.fillna(0)
+
+def timetoseconds(time_str): # Turning q1,q2,q3 times to seconds format
+  time_str = str(time_str)
+  if time_str == "0" or pd.isna(time_str):
+    return 0
+  try:
+    minutes,seconds = time_str.split(':')
+    return int(minutes) * 60 + float(seconds)
+  except ValueError:
+    return 0
+  
+df_main['q1_seconds'] = df_main['q1'].apply(timetoseconds)
+df_main['q2_seconds'] = df_main['q2'].apply(timetoseconds)
+df_main['q3_seconds'] = df_main['q3'].apply(timetoseconds)
+df_main['avg_qualifying_time'] = df_main[['q1_seconds', 'q2_seconds', 'q3_seconds']].mean(axis=1)
 
 #######################
 
@@ -89,9 +121,38 @@ if st.session_state.page_selection == "about":
 elif st.session_state.page_selection == "dataset":
     st.header("📊 Dataset")
 
-    st.write("F1 Dataset") #show each dataset here
-    st.write("") 
-
+    st.markdown("**Formula 1 World Championship (1950 - 2024)** is a dataset made by a user in Kaggle named *Vopani*. It contains 14 csv files ranging from `circuits`, `lap_times`, `pit_stops`, `drivers`, and more. However, in this application we will only use  `drivers.csv`, `qualifying.csv`, `races.csv`, `results.csv` to predict drivers on the podium.") #show each dataset here 
+    st.markdown(""" 
+                - The `driver.csv` contains Formula 1 Drivers basic information from 1950 - 2024.  
+                - The `qualifying.csv` contains the times made by drivers in qualifying sessions.  
+                - The `races.csv` contains the year, race name, and other information of each races in Formula 1.  
+                - The `results.csv` contains information about the initial position of drivers and their final position in the race.  
+                """)
+    st.write("To make a prediction, we will only use multiple features from the csvs such as:")
+    st.markdown("""  
+                - drivers' name, 
+                - race name, 
+                - race year, 
+                - q1, q2, q3 times,  
+                - initial position,  
+                - finishing position,  
+                - average qualifying time
+                 """)
+    st.subheader("Formula 1 Main Dataframe")
+    st.dataframe(df_main,use_container_width = True, hide_index=True)
+    st.subheader("Descriptive Statistics")
+    st.dataframe(df_main.describe(),use_container_width = True)
+    st.markdown(""" 
+                - The average starting grid position is **10.9** and standard deviation of **6.24** which has a high variation.
+                - The average finishing position is **11.203** and standard deviation of *6.291** which has a high variation.
+                - The q1 average time is **88.531** with a standard deviation of **15.467** which has moderate variation.  
+                - The q2 average time is **49.269** with a standard deviation of **44.497** which has a high variation.
+                - The q3 average time is **30.413** with a standard deviation of **42.262** which has extremely high variation.
+                - The mean average qualifying time is **56.071** with a standard deviation of **27.107** which has has a moderately high variation
+                """)
+    # Having a high variation in a dataset means that the model will have a hard time seeing patterns.
+    # Recommendation: Handle outliers, normalize the data. 
+    
     # Your content for your DATASET page goes here
 
 # EDA Page
@@ -116,7 +177,10 @@ elif st.session_state.page_selection == "eda":
 # Data Cleaning Page
 elif st.session_state.page_selection == "data_cleaning":
     st.header("🧼 Data Cleaning and Data Pre-processing")
-
+    # buffer = io.StringIO()
+    # df_main.info(buf=buffer)
+    # info_str = buffer.getvalue()
+    # st.text(info_str)
     # Your content for the DATA CLEANING / PREPROCESSING page goes here
 
 # Machine Learning Page
